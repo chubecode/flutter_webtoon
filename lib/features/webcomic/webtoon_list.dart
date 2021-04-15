e6539c231d876664787d1059e81fc5ce991df201
Tien Nguyen Xuan <xuantien.nguyen@nhn.com>
2021-04-15 23:37:32 +0700

    [UPDATE] webtoon_list.dart

--- lib/features/webcomic/webtoon_list.dart
+++ lib/features/webcomic/webtoon_list.dart
@@ -14,6 +14,7 @@ class WebtoonList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Column(
       children: [
         Row(
@@ -25,38 +26,34 @@ class WebtoonList extends StatelessWidget {
               style: TextStyle(
                 color: Colors.green,
               ),
-            ).onTap(() =>
-                ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text(
-                        sections.first.navigationActionLink.defaultEmpty()))
-                )
-            )
+            ).onTap(() => ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content:
+                    Text(sections.first.navigationActionLink.defaultEmpty()))))
           ],
         ).paddingOnly(left: 10, right: 10),
-
-        ListView.builder(
+        GridView.builder(
             padding: const EdgeInsets.all(8),
             itemCount: sections.length,
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
             itemBuilder: (BuildContext context, int index) {
+              var widthEachItem = size.width / 3 - 28;
               return Container(
-                height: 134,
-                color: Colors.white,
-                child: Row(
+                width: double.infinity,
+                child: Column(
                   children: [
                     CachedNetworkImage(
-                      width: 100,
-                      height: 134,
+                      width: widthEachItem,
+                      height: widthEachItem * 1.34,
                       imageUrl: "http://webtoon.tinyflutterteam.com/static/" +
                           sections[index].items[0].thumb.defaultEmpty(),
                       imageBuilder: (context, imageProvider) => Container(
                         decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
                           image: DecorationImage(
-                              image: imageProvider,
-                              fit: BoxFit.cover,
-                              colorFilter: ColorFilter.mode(
-                                  Colors.white, BlendMode.colorBurn)),
+                            image: imageProvider,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                       placeholder: (context, url) =>
@@ -66,17 +63,24 @@ class WebtoonList extends StatelessWidget {
                     Expanded(
                       child: Text(
                         sections.first.items[index].name.defaultEmpty(),
-                      ),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ).paddingOnly(left: 10, top: 5),
                     ),
                   ],
-                ).onTap(() =>
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text(
-                            sections.first.items[index].deeplink.defaultEmpty()))
-                    )
-                ),
+                ).onTap(() => ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content: Text(sections.first.items[index].deeplink
+                            .defaultEmpty())))),
               ).paddingOnly(top: 5, left: 5, right: 5);
-            }),
+            },
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 3 / 5,
+                crossAxisSpacing: 2)),
       ],
     );
   }
