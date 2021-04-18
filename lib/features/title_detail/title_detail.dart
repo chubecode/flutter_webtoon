2b8815fac31ccba93392bbd98c21918d5b858500
duongdt3 <doanthanhduong11031990@gmail.com>
2021-04-18 13:28:48 +0700

    add header

--- lib/features/title_detail/title_detail.dart
+++ lib/features/title_detail/title_detail.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_webtoon/common/common_widgets.dart';
-import 'package:flutter_webtoon/features/home/home.dart';
-import 'package:flutter_webtoon/features/main/bloc/main_bloc.dart';
-import 'package:flutter_webtoon/features/main/custom_appbar.dart';
-import 'package:flutter_webtoon/features/main/custom_bottom_bar.dart';
-import 'package:flutter_webtoon/features/main/custom_drawer.dart';
-import 'package:flutter_webtoon/features/webcomic/webcomic.dart';
+import 'package:dart_extensions/dart_extensions.dart';
 
 class TitleDetailScreen extends StatelessWidget {
   static final screenName = "/titledetail";
@@ -14,7 +7,147 @@ class TitleDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Center(child: Text("TitleDetailScreen"))),
+      body: Column(
+        children: [
+          _buildHeaderImage(context),
+        ],
+      ),
+    );
+  }
+
+  Container _buildHeaderImage(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.width,
+      child: Stack(
+        children: [
+          Image.network(
+            "https://picsum.photos/300/300",
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width,
+            fit: BoxFit.fill,
+          ),
+          Positioned(
+            bottom: 16,
+            left: 16,
+            right: 16,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [Text("Left")],
+                  ),
+                  Row(
+                    children: [Text("Right")],
+                  )
+                ],
+              ).paddingAll(8),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// class TitleDetailPagerWidget extends StatefulWidget {
+//   const TitleDetailPagerWidget({Key? key}) : super(key: key);
+//   @override
+//   _TitleDetailPagerWidgetState createState() => _TitleDetailPagerWidgetState();
+// }
+
+// class _TitleDetailPagerWidgetState extends State<TitleDetailPagerWidget>
+//     with SingleTickerProviderStateMixin {
+//   final List<Tab> myTabs = <Tab>[
+//     Tab(text: 'LEFT'),
+//     Tab(text: 'RIGHT'),
+//   ];
+
+//   late TabController _tabController;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _tabController = TabController(vsync: this, length: myTabs.length);
+//   }
+
+//   @override
+//   void dispose() {
+//     _tabController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: null,
+//         bottom: TabBar(
+//           controller: _tabController,
+//           tabs: myTabs,
+//         ),
+//       ),
+//       body: TabBarView(
+//         controller: _tabController,
+//         children: myTabs.map((Tab tab) {
+//           final String label = tab.text!.toLowerCase();
+//           return Center(
+//             child: Text(
+//               'This is the $label tab',
+//               style: const TextStyle(fontSize: 36),
+//             ),
+//           );
+//         }).toList(),
+//       ),
+//     );
+//   }
+// }
+
+class TitleDetailScreenScroll extends StatelessWidget {
+  static final screenName = "/titledetail";
+  TitleDetailScreenScroll({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Floating App Bar';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        // No appbar provided to the Scaffold, only a body with a
+        // CustomScrollView.
+        body: CustomScrollView(
+          slivers: <Widget>[
+            // Add the app bar to the CustomScrollView.
+            SliverAppBar(
+              // Provide a standard title.
+              title: Text(title),
+              // Allows the user to reveal the app bar if they begin scrolling
+              // back up the list of items.
+              floating: true,
+              // Display a placeholder widget to visualize the shrinking size.
+              flexibleSpace: Placeholder(),
+              // Make the initial height of the SliverAppBar larger than normal.
+              expandedHeight: 200,
+            ),
+            // Next, create a SliverList
+            SliverList(
+              // Use a delegate to build items as they're scrolled on screen.
+              delegate: SliverChildBuilderDelegate(
+                // The builder function returns a ListTile with a title that
+                // displays the index of the current item.
+                (context, index) => ListTile(title: Text('Item #$index')),
+                // Builds 1000 ListTiles
+                childCount: 1000,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
